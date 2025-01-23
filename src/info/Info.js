55653e6329294a23534ed8e28dc0bcc647a79005
MOHAMMED MOIZ <iammohammedmoiz@gmail.com>
2025-01-23 18:49:24 +0530

    up

--- src/info/Info.js
+++ src/info/Info.js
@@ -112,7 +112,7 @@ export const info = {
     portfolio: [ // This is where your portfolio projects will be detailed
         {
             title: "Project 1",
-            live: "https://paytonpierce.dev", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
+            live: "https://tic-tac-toe-dive.netlify.app/", //this should be a link to the live version of your project, think github pages, netlify, heroku, etc. Or your own domain, if you have it.
             source: "https://github.com/paytonjewell", // this should be a link to the **repository** of the project, where the code is hosted.
             image: mock1
         },
